b4d0a50c9155eaad59be0621f6609ad6ef5093a8
BekYahia <albakrieee@gmail.com>
2021-03-25 04:01:13 +0200

    update config file

---
+++ _nuxt/static/1616637601/projects/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F.\u002F_nuxt\u002Fstatic\u002F1616637601",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002Fprojects",config:{app:{basePath:"\u002F",assetsPath:"\u002F.\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1616637601/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F.\u002F_nuxt\u002Fstatic\u002F1616637601",layout:"default",error:a,state:{},serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F.\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
